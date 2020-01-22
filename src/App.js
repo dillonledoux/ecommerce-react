c9c4ff48d95dd8d8702a57c7c89971dd8d540f10
Dillon LeDoux <dillon.ledoux@me.com>
2020-01-21 22:33:08 -0600

    update menu items to add routing and navigation with withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE {console.log(props.location)}</h1>
+  </div>
+);
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
