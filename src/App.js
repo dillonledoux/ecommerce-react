e02048e43548d8a0d3ad048a17af55378f70b628
Dillon LeDoux <dillon.ledoux@me.com>
2020-01-21 23:46:59 -0600

    add shop page and styling

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = (props) => (
-  <div>
-    <h1>HATS PAGE {console.log(props.location)}</h1>
-  </div>
-);
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
