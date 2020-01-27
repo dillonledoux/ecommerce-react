9fd4ee440f1e4bdfc7fac17915e83135884f9847
Dillon LeDoux <dillon.ledoux@me.com>
2020-01-26 22:36:03 -0600

    add firebase database with user info, add sign up page

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,7 +3,6 @@ import './form-input.styles.sass';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
     <div className='group'>
-        <h2>{otherProps.value}</h2>
         <input className='form-input' onChange={handleChange} {...otherProps} />
         {label ? (
             <label 
