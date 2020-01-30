cfb5819319169abbae5151ba0fef57855cb1aecf
Dillon LeDoux <dillon.ledoux@me.com>
2020-01-30 11:44:23 -0600

    add plus and minus cart item functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, clearItemFromCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: clearItemFromCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM_FROM_CART:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
         default:
             return state;
     }
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,4 +10,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}];
-}
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    if(cartItemToRemove.quantity===1){
+        return clearItemFromCart(cartItems, cartItemToRemove);
+    }
+    
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id 
+        ? {...cartItem, quantity: cartItem.quantity-1} 
+        : cartItem);
+};
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+    const newCart = cartItems.filter(cartItem => cartItem.id!==cartItemToClear.id);
+    return newCart;
+};
