f3c374e89ef0779df1a4d98b5881dc50fd96e23a
Dillon LeDoux <dillon.ledoux@me.com>
2020-02-14 12:48:28 -0600

    add sign in and sign up button custom styled components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './sign-in.styles.sass';
+import { SignInContainer, SignInButton, SignInTitle } from '../sign-in/sign-in.styles';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -39,8 +39,8 @@ class SignIn extends React.Component{
 
     render(){
         return(
-            <div className='sign-in'>
-                <h2> I already have an account</h2>
+            <SignInContainer>
+                <SignInTitle> I already have an account</SignInTitle>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -60,15 +60,15 @@ class SignIn extends React.Component{
                         label='password'
                         required 
                     />
-                    <div className='buttons'>
+                    <SignInButton>
                         <CustomButton type='submit'>Sign in</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                             Sign in with Google
                         </CustomButton>
-                    </div>
+                    </SignInButton>
 
                 </form>
-            </div>
+            </SignInContainer>
         )
     }
 }
